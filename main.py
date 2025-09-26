c28044c6d5c11365caad2e05c0f9b88b61c375eb
Maple <105406712+maplesyrup0423@users.noreply.github.com>
2025-09-26 09:56:52 +0900

    검색 ui 삭제

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ from fastapi import FastAPI, HTTPException, Query
 from fastapi.responses import FileResponse
 import json
 import random
-import os
-from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI(
     title="IT 용어 사전 API",
@@ -11,9 +10,6 @@ app = FastAPI(
     version="1.0.0"
 )
 
-
-# frontend 폴더를 루트 경로에 마운트
-app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
 
 # 데이터 로드
 with open("terms.json", "r", encoding="utf-8") as f:
